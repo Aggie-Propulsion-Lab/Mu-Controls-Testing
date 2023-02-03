dcb9f12871ec8cb06abac3a812a0dd5ee53eb1cb
Hieueee <hieubui457@gmail.com>
2023-02-03 12:53:52 -0800

    ADC
    - made rough draft to set up adc
    - remove the loadcell file because we're not using it anymore.
    - master folder and file for the final code, combining everything together

---
+++ dataPackaging/adcConfig/adcConfig.ino
@@ -0,0 +1,59 @@
+#include <ads1248.h>
+#include <SPI.h>
+
+ads1248 adc1;
+
+/*
+  - START: set HIGH
+  - RESET: set HIGH
+  - CLOK: set LOW
+ */
+int  START = 0;
+int  CS = 0;
+int  DRDY = 0;
+int _RESET = 0;
+
+unsigned long startTime = 0;
+unsigned long elapsedTime = 0;
+
+samplingFreq = 100; 
+samplingPeriod = 1/samplingFreq;
+
+PT1 = 0;
+
+//set up whatever numbers/coefficients needed here for sensors
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while (!Serial) {
+    
+  }
+  Serial.println("Serial online");
+
+  // initialize and calibrate adc
+  adc1.begin();
+  adc1.reset();
+  delay(10);
+
+  // write to registers
+  // configure all the registers here: sampling rate, pga, etc.
+  adc1.setRegisterValue(adress, value); 
+
+  adc1.SendCMD(SELFCAL);
+
+}
+
+void loop() {
+
+  while(record button is hit)
+  {
+    // select mux
+    // get conversion
+    // delay relative to chosen frequency
+    adc1.setRegisterValue(MUX, value);
+    PT1 = adc1.GetConversion();
+    delay(samplingPeriod);  
+  }
+  
+}
